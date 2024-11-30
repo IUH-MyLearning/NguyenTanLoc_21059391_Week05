f398d9cc3a081047f648126c841f1a779d8b948c
colnat412 <nguyentanloc1108@gmail.com>
2024-11-30 15:04:04 +0700

    Initial commit

---
+++ src/main/java/fit/iuh/edu/vn/nguyentanloc_21059391_week05/NguyenTanLoc21059391Week05Application.java
@@ -0,0 +1,13 @@
+package fit.iuh.edu.vn.nguyentanloc_21059391_week05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenTanLoc21059391Week05Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NguyenTanLoc21059391Week05Application.class, args);
+    }
+
+}
---
+++ src/test/java/fit/iuh/edu/vn/nguyentanloc_21059391_week05/NguyenTanLoc21059391Week05ApplicationTests.java
@@ -0,0 +1,13 @@
+package fit.iuh.edu.vn.nguyentanloc_21059391_week05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NguyenTanLoc21059391Week05ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
